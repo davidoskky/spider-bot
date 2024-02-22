75811d12022f8043db3d57de3258b60eada0dad4
davidoskky <davidoskky@yahoo.it>
2024-02-22 21:54:10 +0100

    Add tests for failures

--- tests/test_find_improved_equivalent_position.py
+++ tests/test_find_improved_equivalent_position.py
@@ -5,6 +5,7 @@ import pytest
 from deck import Card, Deck
 from moves_exploration import find_improved_equivalent_position, free_stack
 from spiderSolitaire import Board, Stack
+from util_tests import generate_board_from_string
 
 
 def test_simple_move_one_card():
@@ -331,25 +332,101 @@ def test_should_not_move_equivalent_start_of_stack():
 
 def test_error_14_02():
     """
-    Stack 0: 
-    Stack 1: XX XX 1♠ 3♣ 2♣ 1♣ 9♦ 8♦ 11♠ 10♣ 9♣ 8♣ 7♣ 6♣ 
-    Stack 2: XX XX XX XX XX 13♠ 12♠ 11♣ 10♠ 9♦ 8♦ 7♦ 6♦ 3♣ 2♣ 1♣ 4♣ 3♦ 2♦ 1♦ 
-    Stack 3: XX 5♦ 4♣ 3♣ 2♣ 1♦ 
-    Stack 4: XX XX 5♣ 8♠ 7♥ 6♣ 5♣ 4♣ 13♦ 12♦ 11♠ 10♠ 9♠ 8♠ 7♠ 6♥ 5♥ 4♣ 3♥ 2♣ 
-    Stack 5: 
-    Stack 6: 
-    Stack 7: 12♣ 11♦ 10♣ 9♣ 8♣ 7♠ 6♣ 
-    Stack 8: 13♠ 12♠ 11♠ 10♠ 9♠ 3♦ 11♦ 10♦ 9♥ 
-    Stack 9: 6♦ 
+    Stack 0:
+    Stack 1: XX XX 1♠ 3♣ 2♣ 1♣ 9♦ 8♦ 11♠ 10♣ 9♣ 8♣ 7♣ 6♣
+    Stack 2: XX XX XX XX XX 13♠ 12♠ 11♣ 10♠ 9♦ 8♦ 7♦ 6♦ 3♣ 2♣ 1♣ 4♣ 3♦ 2♦ 1♦
+    Stack 3: XX 5♦ 4♣ 3♣ 2♣ 1♦
+    Stack 4: XX XX 5♣ 8♠ 7♥ 6♣ 5♣ 4♣ 13♦ 12♦ 11♠ 10♠ 9♠ 8♠ 7♠ 6♥ 5♥ 4♣ 3♥ 2♣
+    Stack 5:
+    Stack 6:
+    Stack 7: 12♣ 11♦ 10♣ 9♣ 8♣ 7♠ 6♣
+    Stack 8: 13♠ 12♠ 11♠ 10♠ 9♠ 3♦ 11♦ 10♦ 9♥
+    Stack 9: 6♦
     """
     stacks = [Stack([]) for _ in range(10)]
 
-    stacks[1] = Stack([Card(1, 0)] + [Card(1, 3), Card(3, 2), Card(2, 2), Card(1, 2), Card(9, 1), Card(8, 1), Card(11, 3), Card(10, 2), Card(9, 2), Card(8, 2), Card(7, 2), Card(6, 2)])
-    stacks[2] = Stack([Card(1, 0)] + [Card(13, 3), Card(12, 3), Card(11, 2), Card(10, 3), Card(9, 1), Card(8, 1), Card(7, 1), Card(6, 1), Card(3, 2), Card(2, 2), Card(1, 2), Card(4, 2), Card(3, 1), Card(2, 1), Card(1, 1)])
-    stacks[3] = Stack([Card(1, 0)] + [Card(5, 1), Card(4, 2), Card(3, 2), Card(2, 2), Card(1, 1)])
-    stacks[4] = Stack([Card(1, 0)] + [Card(5, 2), Card(8, 3), Card(7, 0), Card(6, 2), Card(5, 2), Card(4, 2)] + [Card(13, 1), Card(12, 1), Card(11, 3), Card(10, 3), Card(9, 3), Card(8, 3), Card(7, 3), Card(6, 0), Card(5, 0), Card(4, 2), Card(3, 0), Card(2, 2)])
-    stacks[7] = Stack([Card(12, 2), Card(11, 1), Card(10, 2), Card(9, 2), Card(8, 2), Card(7, 3), Card(6, 2)])
-    stacks[8] = Stack([Card(13, 3), Card(12, 3), Card(11, 3), Card(10, 3), Card(9, 3), Card(3, 1), Card(11, 1), Card(10, 1), Card(9, 0)])
+    stacks[1] = Stack(
+        [Card(1, 0)]
+        + [
+            Card(1, 3),
+            Card(3, 2),
+            Card(2, 2),
+            Card(1, 2),
+            Card(9, 1),
+            Card(8, 1),
+            Card(11, 3),
+            Card(10, 2),
+            Card(9, 2),
+            Card(8, 2),
+            Card(7, 2),
+            Card(6, 2),
+        ]
+    )
+    stacks[2] = Stack(
+        [Card(1, 0)]
+        + [
+            Card(13, 3),
+            Card(12, 3),
+            Card(11, 2),
+            Card(10, 3),
+            Card(9, 1),
+            Card(8, 1),
+            Card(7, 1),
+            Card(6, 1),
+            Card(3, 2),
+            Card(2, 2),
+            Card(1, 2),
+            Card(4, 2),
+            Card(3, 1),
+            Card(2, 1),
+            Card(1, 1),
+        ]
+    )
+    stacks[3] = Stack(
+        [Card(1, 0)] + [Card(5, 1), Card(4, 2), Card(3, 2), Card(2, 2), Card(1, 1)]
+    )
+    stacks[4] = Stack(
+        [Card(1, 0)]
+        + [Card(5, 2), Card(8, 3), Card(7, 0), Card(6, 2), Card(5, 2), Card(4, 2)]
+        + [
+            Card(13, 1),
+            Card(12, 1),
+            Card(11, 3),
+            Card(10, 3),
+            Card(9, 3),
+            Card(8, 3),
+            Card(7, 3),
+            Card(6, 0),
+            Card(5, 0),
+            Card(4, 2),
+            Card(3, 0),
+            Card(2, 2),
+        ]
+    )
+    stacks[7] = Stack(
+        [
+            Card(12, 2),
+            Card(11, 1),
+            Card(10, 2),
+            Card(9, 2),
+            Card(8, 2),
+            Card(7, 3),
+            Card(6, 2),
+        ]
+    )
+    stacks[8] = Stack(
+        [
+            Card(13, 3),
+            Card(12, 3),
+            Card(11, 3),
+            Card(10, 3),
+            Card(9, 3),
+            Card(3, 1),
+            Card(11, 1),
+            Card(10, 1),
+            Card(9, 0),
+        ]
+    )
     stacks[9] = Stack([Card(6, 1)])
 
     for stack in stacks:
@@ -364,3 +441,41 @@ def test_error_14_02():
     result = find_improved_equivalent_position(board)
 
     assert result != [], "Should move 7 to 4"
+
+
+def test_error_22_02():
+    board = generate_board_from_string(
+        """Stack 0: XX XX XX XX 2♥ 1♥ 
+Stack 1: XX 11♣ 4♦ 3♦ 2♦ 1♦ 
+Stack 2: 6♦ 6♥ 5♥ 4♥ 3♣ 2♣ 1♣ 
+Stack 3: XX XX XX XX 12♥ 11♠ 10♠ 9♠ 13♦ 12♦ 11♦ 10♥ 9♥ 8♥ 7♥ 
+Stack 4: XX XX XX XX 11♥ 10♥ 
+Stack 5: XX XX XX 7♦ 2♦ 10♣ 
+Stack 6: XX XX XX XX 3♠ 2♠ 1♠ 4♥ 6♣ 12♦ 11♦ 10♦ 9♠ 
+Stack 7: 6♦ 
+Stack 8: XX XX XX 1♦ 11♥ 12♥ 11♣ 10♣ 9♣ 8♦ 7♠ 6♠ 5♠ 4♠ 3♠ 2♠ 2♣ 1♠ 
+Stack 9: XX XX 13♥ 12♣ 11♠ 10♠ 
+"""
+    )
+
+    result = find_improved_equivalent_position(board)
+    assert result != [], "Should move 3 to 4"
+    assert len(result) == 1, "Should move 3 to 4"
+
+
+def test_failure_22_02():
+    board = generate_board_from_string(
+        """Stack 0: XX XX XX 13♠ 12♠ 11♣ 10♣ 9♣ 
+Stack 1: 10♦ 4♥ 3♥ 
+Stack 2: XX 13♦ 12♦ 11♦ 10♦ 9♦ 8♦ 7♦ 6♦ 5♦ 4♦ 
+Stack 3: XX XX 8♣ 7♥ 3♦ 2♠ 3♠ 2♦ 1♠ 13♠ 
+Stack 4: 8♠ 
+Stack 5: XX XX XX 5♠ 4♦ 3♦ 2♦ 1♦ 10♠ 9♠ 8♣ 7♦ 6♣ 5♦ 1♦ 
+Stack 6: XX XX XX 13♦ 12♠ 11♦ 13♣ 12♣ 11♣ 10♣ 9♥ 8♦ 7♣ 6♣ 5♣ 4♣ 3♣ 2♣ 
+Stack 7: 4♠ 3♠ 2♠ 1♠ 
+Stack 8: 
+Stack 9: 3♣ 2♣ 1♣ 13♥ 12♥ 11♥ 10♠ 9♠ 8♠ 7♠ 6♦ """
+    )
+
+    result = find_improved_equivalent_position(board)
+    assert result != [], "Should move 0 to 6"
