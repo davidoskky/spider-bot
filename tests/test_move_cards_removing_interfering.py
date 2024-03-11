01bb4e5ee14528882064513069521c0d02849ca6
davidoskky <davidoskky@yahoo.it>
2024-03-11 19:30:37 +0100

    Add failing test

--- tests/test_move_cards_removing_interfering.py
+++ tests/test_move_cards_removing_interfering.py
@@ -3,7 +3,8 @@ import logging
 import pytest
 
 from deck import Card, Deck
-from moves_exploration import move_cards_removing_interfering
+from moves_exploration import (move_card_splitting,
+                               move_cards_removing_interfering)
 from spiderSolitaire import Board, Stack
 from util_tests import generate_board_from_string
 
@@ -19,7 +20,7 @@ def test_switch_sequences_can_sequence():
     stacks = tuple(stacks)
     board = Board(stacks=stacks, deck=Deck(), completed_stacks=0)
 
-    result = move_cards_removing_interfering(board, 1, 2, 1)
+    result = move_cards_removing_interfering(board, 1, 2, 1, 1)
     assert result != [], "Should not return an empty list"
     assert len(result) == 3, "Should take three moves"
 
@@ -35,7 +36,7 @@ def test_move_card_no_clearing_needed():
     stacks = tuple(stacks)
     board = Board(stacks=stacks, deck=Deck(), completed_stacks=0)
 
-    result = move_cards_removing_interfering(board, 1, 2, 1)
+    result = move_cards_removing_interfering(board, 1, 2, 1, 0)
     assert result != [], "Should not return an empty list"
     assert len(result) == 2, "Should take two moves"
 
@@ -54,7 +55,7 @@ def test_switch2():
 
     board = Board(stacks=tuple(stacks), deck=Deck(), completed_stacks=0)
 
-    result = move_cards_removing_interfering(board, 0, 2, 2)
+    result = move_cards_removing_interfering(board, 0, 2, 2, 4)
 
     assert result != []
 
@@ -114,7 +115,7 @@ def test_error_13_02():
 
     board = Board(stacks=tuple(stacks), deck=Deck(), completed_stacks=0)
 
-    result = move_cards_removing_interfering(board, 0, 3, 4)
+    result = move_cards_removing_interfering(board, 0, 3, 4, 5)
 
     logging.debug(result)
     assert result != [], "A solution is available"
@@ -136,6 +137,25 @@ def test_error_14_02():
     """
     )
 
-    result = move_cards_removing_interfering(board, 7, 4, 1)
+    result = move_cards_removing_interfering(board, 7, 4, 1, 3)
 
     assert result != [], "Should move 7 to 4"
+
+
+def test_do_not_move_11_03():
+    board = generate_board_from_string(
+        """Stack 0: XX XX XX XX XX 5♠ 4♣ 3♦ 2♦ 1♣
+Stack 1: XX XX XX 7♠ 8♦ 
+Stack 2: XX XX XX 5♠ 4♥ 13♥ 12♥ 11♥ 10♥ 9♥ 8♥ 7♥ 6♥ 
+Stack 3: 8♦ 7♣ 7♠ 6♠ 
+Stack 4:  
+Stack 5: XX XX 12♥ 5♣ 4♠ 3♦ 1♠ 
+Stack 6: XX XX XX XX 13♦ 10♦ 9♣ 8♥ 
+Stack 7: XX XX 13♣ 12♣ 11♣ 10♣ 9♠ 8♣ 1♦ 4♦ 3♥ 
+Stack 8: XX XX XX 10♠ 9♠ 8♠ 1♣ 5♣ 
+Stack 9: XX XX 4♠ 3♠ 2♠ 1♠ 11♠ 13♦ """
+    )
+
+    result = move_cards_removing_interfering(board, 0, 8, 6, 8)
+    assert result != [], "Should move"
+    assert len(result) == 5
