9935668727acf92b670de53a79459768dfbadc06
davidoskky <davidoskky@yahoo.it>
2024-03-22 18:57:54 +0100

    Identify seeds which can be won

--- runBot.py
+++ runBot.py
@@ -6,7 +6,13 @@ from tabnanny import verbose
 from spiderSolitaire import SpiderSolitaire
 from spiderSolitaireBot import SpiderSolitaireBot
 
-bot = SpiderSolitaireBot(SpiderSolitaire())
+bot = SpiderSolitaireBot(SpiderSolitaire(seed=66))
 # print(f"Game solveable {bot.gameSolvable()}")
 # bot.play_bfs()
 bot.play_heuristic(verbose=1)
+
+for i in range(1000):
+    bot = SpiderSolitaireBot(SpiderSolitaire(seed=i))
+    bot.play_heuristic(verbose=0)
+    if bot.game.is_game_won():
+        print(f"Game won, seed: {i}")
